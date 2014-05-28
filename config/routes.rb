e891fbcc0127f83e10e3e8bd9bc0b9017fde6605
Gonçalo Dias da Silva <gopdiasdasilva@gmail.com>
2014-05-28 01:09:46 +0100

    Criada página do calendário e acrescentado calendário responsive-calendar.

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'contents#index'
+   get '/calendar' => 'pages#calendar'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
